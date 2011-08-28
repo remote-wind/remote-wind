846613120991b374a2e8855414caaf7bab24daef
Karl-Petter Åkesson <karl-petter@yelloworb.com>
2011-08-28 19:58:54 +0200

    removed geokit

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,5 +1,9 @@
+#include Geokit::Geocoders
+
 class Station < ActiveRecord::Base
+#  acts_as_mappable  :default_units => :kms,
+#                    :lat_column_name => :lat,
+#                    :lng_column_name => :lon
   has_many :measures
-  acts_as_mappable  :lat_column_name => :lat,
-                    :lng_column_name => :lon
+  
 end
