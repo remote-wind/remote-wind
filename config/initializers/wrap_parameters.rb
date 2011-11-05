dafe59082dd5ca1966e7971a5fb229f2b8989464
Karl-Petter Åkesson <karl-petter@yelloworb.com>
2011-11-05 14:17:05 +0100

    Config to not include root elements in json

--- config/initializers/wrap_parameters.rb
+++ config/initializers/wrap_parameters.rb
@@ -3,8 +3,6 @@
 # This file contains settings for ActionController::ParamsWrapper which
 # is enabled by default.
 
-# Enable parameter wrapping for JSON. You can disable this by setting :format to an empty array.
-ActionController::Base.wrap_parameters :format => [:json]
 
 # Disable root element in JSON by default.
 if defined?(ActiveRecord)
