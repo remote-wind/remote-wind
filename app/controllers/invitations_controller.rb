e8589489067f77b5bb7427176edacc01c749d37c
Karl-Petter Åkesson <karl-petter@yelloworb.com>
2012-10-07 20:48:23 +0200

    Fixed two issues with the invitations system, first non authorized users was not allowed to accept invitation(ie no one) and secondly I had to set a timezone to be able to set the user password. Dont understand why, only stations has timezone.

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,11 +1,17 @@
 class InvitationsController < Devise::InvitationsController
-  skip_authorization_check :only => [:accept]
+  skip_authorization_check :only => [:accept, :edit, :update]
   def new
     if cannot?( :invite, User )
       raise CanCan::AccessDenied
     else
       super
     end
+  end
+  
+  def update
+    zone = ActiveSupport::TimeZone.create("CET")
+    Time.zone = zone unless zone.nil?
+    super
   end
   
   def create
