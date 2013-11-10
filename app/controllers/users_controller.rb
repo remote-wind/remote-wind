243ec71d5879e754b46d988da599986a27198c32
Max Calabrese <max.calabrese@ymail.com>
2013-11-10 15:17:39 +0100

    add auth for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,13 @@
 class UsersController < ApplicationController
+  load_and_authorize_resource
+  before_filter :authenticate_user!
+
+  # GET /users/:id
   def show
     @user = User.find(params[:id])
   end
 
+  # GET /users/
   def index
     @users = User.all
   end
