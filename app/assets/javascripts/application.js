remote-wind/remote-wind

commit 961a90b6b338b8432d28043509eae96e9edf1674

refactored google map implemation to be more flexible

app/assets/javascripts/application.js
@@ -23,71 +23,3 @@ window.console = window.console||{
     info: function(){},
     error: function(){}
 };
-
-$(function(){
-    $(document).foundation();
-    $('body').on('maps-api-loaded', function(){
-
-        var $map_canvas = $('#stations_map');
-        if ($map_canvas.length){
-            (function() {
-
-                var mapOptions = {
-                    center: new google.maps.LatLng(-34.397, 150.644),
-                    zoom: 10,
-                    mapTypeId: google.maps.MapTypeId.ROADMAP
-                };
-                var map = new google.maps.Map(document.getElementById("stations_map"),
-                    mapOptions);
-
-                map.infoWindow = new google.maps.InfoWindow();
-
-                $map_canvas.on('new-user-position', function(event, lat, lng){
-                    map.panTo(new google.maps.LatLng(lat, lng));
-                });
-
-                $map_canvas.on('station-data-loaded', function(event, data){
-                    $(data).each(function(){
-                        var $row, marker, infowindow, text;
-                        $row = $('#station-'+ this.id);
-                        marker = new google.maps.Marker({
-                            position: new google.maps.LatLng(this.latitude, this.longitude),
-                            map: map,
-                            title: this.name
-                        });
-                        marker.content = '<p><a href="'+ this.path +'">'+ this.name +'</a></p>';
-                        google.maps.event.addListener(marker, 'click', function(){
-                            map.infoWindow.close();
-                            map.panTo(marker.position);
-                            map.infoWindow.setPosition(marker.position);
-                            map.infoWindow.setContent(marker.content);
-                            map.infoWindow.open(map, marker);
-                        });
-                        $row.data('marker', marker);
-                    });
-                });
-
-                $('.stations').on('click', '.station', function(e){
-                    var marker = $(this).data('marker');
-                    map.panTo(marker.position, 15);
-                });
-
-                var stations = $.getJSON('/stations', function(data, status){
-                    $map_canvas.trigger('station-data-loaded', [data]);
-                });
-
-                if(navigator.geolocation) {
-                    return navigator.geolocation.getCurrentPosition(
-                        function(p) {
-                            $map_canvas.trigger('new-user-position',[ p.coords.latitude, p.coords.longitude ]);
-                        }
-                    );
-                };
-            }());
-        }
-    });
-});
-
-
-
-

app/assets/javascripts/stations.js
@@ -1,6 +1,56 @@
 $(function () {
 
     /**
+     * Really generic google maps implimention that uses data attributes and good old html
+     * to place markers
+     */
+    (function(){
+        var $map_canvas = $('#map_canvas');
+        var map;
+
+        $('body').on('maps-api-loaded', function(){
+            if ($map_canvas.length){
+                $map_canvas.trigger('map.init');
+            }
+        });
+
+        $map_canvas.on('map.init', function(){
+            var $markers, mapOptions, map;
+            $markers = $map_canvas.find('.marker').clone();
+            $map_canvas.empty();
+
+            mapOptions = {
+                center: new google.maps.LatLng($map_canvas.data('lat'), $map_canvas.data('lon')),
+                zoom: 10,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+
+            map = new google.maps.Map($map_canvas[0],
+                mapOptions);
+
+            map.infoWindow = new google.maps.InfoWindow();
+
+            $markers.each(function(){
+                var marker = new google.maps.Marker({
+                    position: new google.maps.LatLng($(this).data('lat'), $(this).data('lon')),
+                    map: map,
+                    title: $(this).find('.title').text(),
+                    content: $(this).html()
+                });
+
+                google.maps.event.addListener(marker, 'click', function(){
+                    map.infoWindow.close();
+                    map.panTo(marker.position);
+                    map.infoWindow.setPosition(marker.position);
+                    map.infoWindow.setContent(marker.content);
+                    map.infoWindow.open(map, marker);
+                });
+            });
+        });
+    }());
+
+
+    /**
      * Station chart
      */
     (function(){

app/assets/stylesheets/modules.css.scss
@@ -3,3 +3,7 @@
   background-color: #778899;
   img{max-width: inherit;}
 }
+
+.map-canvas  {
+  height: 360px;
+}

app/helpers/stations_helper.rb
@@ -4,4 +4,8 @@ module StationsHelper
     params[:action] == 'index'
   end
 
+  def speed_min_max(measure)
+    "%{speed} (%{min_wind_speed}/%{max_wind_speed})" % measure
+  end
+
 end

app/views/stations/index.html.erb
@@ -3,8 +3,17 @@
 <% unless is_home? %>
 <h1>Wind stations</h1>
 <% end %>
-<div class="stations_map small-12 large-12 columns" id="stations_map">
+
+<% unless @stations.empty?  %>
+<div class="stations_map small-12 large-12 columns" id="map_canvas" data-lat="<%= @stations.first.lat %>" data-lon="<%= @stations.first.lon %>">
+  <% @stations.each do |station| %>
+  <div class="marker" data-lat="<%= station.lat %>" data-lon="<%= station.lon %>">
+    <h3 class="title"><%= station.name %></h3>
+  </div>
+  <% end %>
 </div>
+<% end %>
+
 <table class="stations large-8 columns">
   <thead>
     <tr>

app/views/stations/show.html.erb
@@ -3,8 +3,15 @@
 <p>Owned by <%= @station.user.email %></p>
 <% end %>
 
-<% if @measures %>
+<div class="row">
+  <div class="map-canvas small-12 large-12 columns" data-lat="<%= @station.lat %>" data-lon="<%= @station.lon %>" id="map_canvas">
+    <div class="marker" data-lat="<%= @station.lat %>" data-lon="<%= @station.lon %>">
+      <h3 class="title"><%= @station.name %></h3>
+    </div>
+  </div>
+</div>
 
+<% if @measures %>
 <div class="section-container auto" data-section>
   <section class="active">
     <p class="title" data-section-title><a href="#panel1">Table</a></p>
@@ -33,7 +40,6 @@
     </div>
   </section>
 </div>
-
 <% end %>
 
 <% if can? :manage, Station %>

commit 6432afe1eca570e607f59970c4db07cbc5f8e92d

fixes js issue caused by 961a90b6b

app/assets/javascripts/application.js
@@ -23,3 +23,7 @@ window.console = window.console||{
     info: function(){},
     error: function(){}
 };
+
+$(function(){
+    $(document).foundation();
+});

app/assets/stylesheets/foundation_and_overrides.scss
@@ -1136,4 +1136,3 @@ $default-float: left;
 @import 'type';
 @import 'navigation';
 @import 'modules';
-@import 'foundation';