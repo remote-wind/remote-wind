066ba926185102a65c55d0da831488e1ef8e097a
Max Calabrese <max.calabrese@ymail.com>
2013-11-18 14:54:26 +0100

    add view specs for maps

--- spec/views/pages/home.html.erb_spec.rb
+++ spec/views/pages/home.html.erb_spec.rb
@@ -29,10 +29,15 @@ describe "pages/home" do
     rendered
   end
 
-
-
   it { should have_content /REMOTE WIND/i}
   it { should have_selector ".station", :exact => 3 }
   it { should have_selector ".speed", text: measure.speed }
   it { should have_selector ".direction", text: "#{measure.direction} (#{measure.compass_point})"}
+
+  describe "map" do
+    it { should have_selector "#map_canvas" }
+    it { should have_selector '#map_canvas .marker', exact: stations.length }
+    it { should have_selector "#map_canvas .marker[data-lat='#{stations[0].lat}']" }
+    it { should have_selector "#map_canvas .marker[data-lon='#{stations[0].lon}']" }
+  end
 end
--- spec/views/stations/index.html.erb_spec.rb
+++ spec/views/stations/index.html.erb_spec.rb
@@ -18,49 +18,42 @@ describe "stations/index" do
   
   before(:each) do
     stub_user_for_view_test
-    assign(:stations, [
-      stub_model(Station),
-      stub_model(Station)
-    ])
-
   end
 
+  subject {
+    render
+    rendered
+  }
+
+  it { should have_selector '.speed' }
+  it { should have_selector '.direction' }
+  it { should match /[s|S]tations/ }
+  it { should have_selector('.station', :minimum => 2) }
+
   context "when not an admin" do
-    subject {
-      render
-      rendered
-    }
-
-    it { should have_selector '.speed' }
-    it { should have_selector '.direction' }
-
-    it { should match /[s|S]tations/ }
-    it { should have_selector('.station', :minimum => 2) }
     it { should_not have_link 'Edit' }
     it { should_not have_link 'Delete' }
   end
 
   context "when an admin" do
-
     subject {
       @ability.can :manage, Station
       render
       rendered
     }
-
     it { should have_link 'Edit' }
     it { should have_link 'Delete' }
   end
 
   describe "breadcumbs" do
-    subject {
-      render
-      rendered
-    }
-
     it { should have_selector '.breadcrumbs .root', text: 'Home' }
     it { should have_selector '.breadcrumbs .current', text: 'Stations' }
-
   end
 
+  describe "map" do
+    it { should have_selector "#map_canvas" }
+    it { should have_selector '#map_canvas .marker', exact: stations.length }
+    it { should have_selector "#map_canvas .marker[data-lat='#{stations[0].lat}']" }
+    it { should have_selector "#map_canvas .marker[data-lon='#{stations[0].lon}']" }
+  end
 end
