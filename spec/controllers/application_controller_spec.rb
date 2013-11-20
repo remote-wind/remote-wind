13926e6738bf46ea46d5b085eb2a9cd4ed2779f6
Max Calabrese <max.calabrese@ymail.com>
2013-11-20 20:10:53 +0100

    added specs to check for FB app id & secret in ENV

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -3,12 +3,7 @@ require 'spec_helper'
 describe ApplicationController do
 
   # Sanity check to make sure enviroment is set up
-  describe "enviromental variables"  do
-
-    it "should have email" do
-      expect(ENV['REMOTE_WIND_EMAIL'].blank?).to be_false
-    end
-
+  describe "enviroment"  do
 
   end
 end
