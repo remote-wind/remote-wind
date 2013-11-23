7d11546f311621fef43d63d256728bd8afc7d802
Max Calabrese <max.calabrese@ymail.com>
2013-11-23 22:12:53 +0100

    moved handling arduino params to controller. Params are a controller concern

--- app/controllers/measures_controller.rb
+++ app/controllers/measures_controller.rb
@@ -14,6 +14,8 @@ class MeasuresController < ApplicationController
 
   # POST /measures
   def create
+
+    # @todo write spec and use normalize_ardiuno_params
     @measure = Measure.new(measure_params)
 
     respond_to do |format|
@@ -36,22 +38,45 @@ class MeasuresController < ApplicationController
     end
   end
 
+  @@dict = {
+      :speed => :s,
+      :direction => :d,
+      :station_id => :i,
+      :max_wind_speed => :max,
+      :min_wind_speed => :min
+  }
+
+  def normalize_ardiuno_params params
+    params[:speed] = params[:speed] * 10
+    params[:max_wind_speed] = params[:max_wind_speed] * 10
+    params[:min_wind_speed] = params[:min_wind_speed] * 10
+    params[:direction] = params[:direction]  / 10
+  end
+
+  def params_to_long_form params
+    mappings = @@dict.invert.with_indifferent_access
+    Hash[params.map {|k, v| [mappings[k] || k, v] }]
+  end
+
+  def params_to_short_form params
+    mappings = @@dict.with_indifferent_access
+    Hash[params.map {|k, v| [mappings[k] || k, v] }]
+  end
+
   private
   # Never trust parameters from the scary internet, only allow the white list through.
   def measure_params
     ## Handle short form params
     if params[:m]
-      sanitized = params.require(:m).permit(:i,:s,:s,:d,:s,:min,:max,:t)
-      return Measure.params_to_long_form(sanitized.to_hash)
+      sanitized = params.require(:m).permit(:i,:s,:d,:min,:max)
+      return params_to_long_form(sanitized.to_hash)
     end
     params.require(:measure).permit(
-        :m, :id, :speed, :station_id, :direction, :speed, :min_wind_speed, :max_wind_speed, :temperature
+        :id, :station_id, :direction, :speed, :min_wind_speed, :max_wind_speed
     )
   end
 
   def set_measure
     @measure = Measure.find(params[:id])
   end
-
-
 end
--- app/models/measure.rb
+++ app/models/measure.rb
@@ -5,27 +5,10 @@ class Measure < ActiveRecord::Base
   validates :speed, :direction, :max_wind_speed, :min_wind_speed,
             :numericality => { :allow_blank => true }
 
-  @@dict = {
-      :speed => :s,
-      :direction => :d,
-      :station_id => :i,
-      :max_wind_speed => :max,
-      :min_wind_speed => :min,
-      :temperature => :t
-  }
-
   def compass_point
     Geocoder::Calculations.compass_point(self.direction)
   end
 
-  def self.params_to_long_form params
-    mappings = @@dict.invert.with_indifferent_access
-    Hash[params.map {|k, v| [mappings[k] || k, v] }]
-  end
 
-  def self.params_to_short_form params
-    mappings = @@dict.with_indifferent_access
-    Hash[params.map {|k, v| [mappings[k] || k, v] }]
-  end
 
 end
