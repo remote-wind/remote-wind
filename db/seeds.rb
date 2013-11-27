6b9cea9a7df7a7f1869a6ca1b2e32ec6d6da1702
Max Calabrese <max.calabrese@ymail.com>
2013-11-27 20:38:18 +0100

    more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,41 +11,68 @@ Timezone::Configure.begin do |c|
   c.username = ENV['REMOTE_WIND_GEONAMES']
 end
 
-admin = User.new
-admin.email = ENV["REMOTE_WIND_EMAIL"]
-admin.password = ENV["REMOTE_WIND_PASSWORD"]
-admin.password_confirmation = ENV["REMOTE_WIND_PASSWORD"]
-admin.add_role(:admin)
-admin.save!
+unless User.find_by_email(ENV["REMOTE_WIND_EMAIL"])
 
-users = %w[joe.smoe@example.com john.doe@example.com test@example.com]
+  admin = User.find_or_create_by_email(
+      email: ENV["REMOTE_WIND_EMAIL"],
+      password: ENV["REMOTE_WIND_PASSWORD"],
+      password_confirmation: ENV["REMOTE_WIND_PASSWORD"]
+  )
+  admin.add_role(:admin)
 
-users.each do |email|
-  user = User.new
-  user.email = email
-  user.password = 'kitekite'
-  user.password_confirmation = 'kitekite'
-  user.save!
+  puts "Hello my new master #{ENV['REMOTE_WIND_EMAIL']}, I have created an account just for you. The password in the one you set in REMOTE_WIND_PASSWORD
+ \n\n"
+
 end
 
-# Tegefjall 0.3 m/s 183.2° (63.4017,12.97256) IMEI: 354476020409714 all measures
-# Mullfjället (63.42258,12.95487) IMEI: 3129831239 all measures
-# Ullådalen (63.4321,13.00011) IMEI: 354476020409715 all measures
-stations= Station.create([{:name => 'Tegefjäll', :hw_id =>'354476020409714', :lat =>'63.4017', :lon =>'12.97256', :user => User.find_by_email(ENV["REMOTE_WIND_EMAIL"])},
-                          {:name => 'Mullfjället', :hw_id =>'354476020409715', :lat =>'63.42258', :lon =>'12.95487'},
-                          {:name => 'Ullådalen', :hw_id =>'354476020409716', :lat =>'63.4321', :lon =>'13.00011'},
-                         ])
+def svenne_generator
 
-stations.each do |station|
+  fn = %w[johan nils carl-olof sofie anna kristina sara olof bengt tommy]
+  ln1 = %w[berg wall aker holm]
+  ln2 = %w[gren kvist strom stedt]
 
-  rand(1..15).times do
+  email =  fn.sample + "." + ln1.sample + ln2.sample + "@example.com"
+end
 
-    station.measures.create(
-        :speed => rand(0..40),
-        :direction => rand(0..360),
-        :max_wind_speed => rand(0..40),
-        :min_wind_speed => rand(0..40)
-    )
+puts "Spawing random users \n"
+users = (1..50).to_a.map! do
+  print "."
+  User.find_or_create_by_email svenne_generator
+end
+
+unless Station.any?
+  puts "\nCreating stations\n"
+  stations = [
+      {name: 'Tegefjäll', hw_id: '354476020409714', lat: '63.4017', lon:'12.97256'},
+      {name: 'Mullfjället', hw_id: '354476020409715', lat: '63.42258', lon: '12.95487'},
+      {name: 'Ullådalen', hw_id: '354476020409716', lat: '63.4321', lon: '13.00011'},
+      {name: 'Frösön', hw_id: '3544760204324716', lat: 63.206557, lon: 14.446625},
+      {name: 'Näsbokrok', hw_id: '123123123', lat: 57.336714, lon: 12.067055 },
+      {name: 'Mörbylånga N', hw_id: '12312312322', lat: 56.603728, lon: 16.418273}
+  ].map! do |s|
+    print s[:name]
+    puts " "
+    Station.find_or_create_by_hw_id(s)
   end
+end
 
+puts "\nSeeding stations with random measurements\n"
+Station.all.each do |station|
+  puts station[:name]
+  rnd = Random.new.rand(5..35)
+  t = Time.now - (rnd * 1000)
+  rnd.times do
+    speed = Random.new.rand(0..30)
+    measure = station.measures.create({
+        station_id: station.id,
+        direction: Random.new.rand(0..360),
+        speed: speed,
+        min_wind_speed: Random.new.rand(0..speed),
+        max_wind_speed: Random.new.rand(speed..speed + 15)
+    })
+    t = t + 1000
+    measure.created_at = t
+    print "."
+  end
+  puts "\n"
 end
