3d06897db8b4901fd1c050ec13ae03aea066a122
Max Calabrese <max.calabrese@ymail.com>
2013-12-28 20:20:37 +0100

    add test for measure ordering and fix wierd issue where spec causes SIGSEGV

--- spec/views/measures/_table.html.erb_spec.rb
+++ spec/views/measures/_table.html.erb_spec.rb
@@ -5,25 +5,24 @@ describe "measures/_table" do
   let(:station){ create(:station) }
   let(:measures) do
     [*1..3].map! do |i|
-      measure = create(:measure, station: station)
-      measure.update_attribute(:created_at, (i - 1).hours.ago )
-
-      measure
+      build_stubbed(:measure, station: station, created_at: Time.now - i.hours)
     end
   end
 
-  before(:each) do
-    stub_user_for_view_test
-    assign(:measures, measures)
+  before :each do
     assign(:station, station)
+    assign(:measures, measures)
   end
 
-  subject(:rendered) do
+  subject do
     render
-    Capybara.string(rendered)
+    rendered
   end
 
-  it { should have_selector 'tr', exact: 3 }
-  it { should have_selector 'tr:first .created_at', text: measures.last.created_at.strftime( "%H:%M" ) }
+  it { should have_selector ".measure", exact: measures.length }
+
+  it "reverses the order of measures" do
+    expect(subject).to have_selector ".measure:first .created_at", text: time_date_hours_seconds(station.time_to_local(measures.last.created_at))
+  end
 
 end
