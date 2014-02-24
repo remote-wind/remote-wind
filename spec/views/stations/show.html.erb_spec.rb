72c9d1c6132dc478d5e5fd9b3f60ce5192fd43ad
Max Calabrese <max.calabrese@ymail.com>
2014-02-24 21:23:22 +0100

    Add speed calibration to meta #56

--- spec/views/stations/show.html.erb_spec.rb
+++ spec/views/stations/show.html.erb_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "stations/show" do
 
   let (:station) {
-    build_stubbed(:station)
+    build_stubbed(:station, timezone: "Brisbane", speed_calibration: 0.5143)
   }
 
   let (:measures) do
@@ -12,7 +12,6 @@ describe "stations/show" do
     measures << build_stubbed(:measure, station: station, created_at: Time.new(2000) - 5.minutes)
     measures.sort_by! { |m| m.created_at }
   end
-
 
   before(:each) do
     stub_user_for_view_test
@@ -33,11 +32,7 @@ describe "stations/show" do
   end
 
   context "when an admin" do
-    subject {
-      @ability.can :manage, Station
-      render
-      rendered
-    }
+    before { @ability.can :manage, Station }
     it { should have_link 'Edit' }
     it { should have_link 'Clear all measures for this station' }
   end
@@ -52,6 +47,16 @@ describe "stations/show" do
     it { should have_selector "#map_canvas .marker .title", text: station.name }
     it { should have_selector "#map_canvas .marker[data-lat='#{station.lat}']" }
     it { should have_selector "#map_canvas .marker[data-lon='#{station.lon}']" }
+  end
+
+  describe "meta" do
+    it { should have_selector ".station-meta .created-at td:last", text: station.time_to_local(station.created_at) }
+    it { should have_selector ".station-meta .updated-at td:last", text: station.time_to_local(station.updated_at) }
+    it { should have_selector ".station-meta .latitude td:last", text: station.latitude }
+    it { should have_selector ".station-meta .longitude td:last", text: station.longitude }
+    it { should have_selector ".station-meta .timezone td:last", text: station.timezone }
+    it { should have_selector ".station-meta .visible td:last", text: "yes" }
+    it { should have_selector ".station-meta .speed-calibration td:last", text: 0.51 }
   end
 
 end
