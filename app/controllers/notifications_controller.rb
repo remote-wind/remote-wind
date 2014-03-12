f176fc117a1d4adcbbb5adefbffc16bcea93df00
Max Calabrese <max.calabrese@ymail.com>
2014-03-12 19:05:34 +0100

    refactor notifications_controller to ensure actions are scoped to user

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,16 +4,18 @@ class NotificationsController < ApplicationController
 
   skip_before_filter :get_notifications, only: [:mark_all_as_read]
 
+  before_filter :set_user
+
   # Display notifications belonging to the currently logged in user.
   # GET /notifications
   def index
-    @user = current_user
-    @notifications = Notification
-            .where(user_id: @user.id)
+
+    @notifications = @user.notifications
             .order(created_at: :desc)
             .paginate(page: params[:page])
 
-    @title = "Inbox(#{ @notifications.count })"
+    @title = "Inbox"
+    @title += " - page #{params[:page]}" unless params[:page].nil?
 
     # Render response before marking notifications as read
     respond_to do |format|
@@ -26,10 +28,9 @@ class NotificationsController < ApplicationController
   # PATCH /notifications/mark_all_as_read
   def mark_all_as_read
 
-    @notifications = Notification.where(user_id: current_user.id, read: false)
-    updated = @notifications.update_all(read: true)
+    @notifications = @user.notifications.where(read: false)
 
-    if updated > 0
+    if @notifications.update_all(read: true) > 0
       flash[:success] = 'All notifications have been marked as read.'
       redirect_to action: :index
     else
@@ -41,7 +42,7 @@ class NotificationsController < ApplicationController
 
   # DESTROY /notifications/:id
   def destroy
-    @notification = Notification.find(params[:id])
+    @notification = @user.notifications.find(params[:id])
     @notification.destroy
     flash[:success] = 'Notification deleted.'
     redirect_to action: :index
@@ -50,12 +51,10 @@ class NotificationsController < ApplicationController
   # DESTROY /notifications
   def destroy_all
 
-    conditions = { user_id: current_user.id }
-    conditions[:read] = true if params[:condition] == 'read'
+    @notifications = @user.notifications
+    @notifications = @notifications.where(read: true) if params[:condition] == 'read'
 
-    @notifications = Notification.where(conditions)
-
-    # Use time input to limit delete chonographically
+    # Use time input to limit chonographically
     if (!params[:time].nil? && !params[:time_unit].nil?)
       time = params[:time].to_i
       unit = ['days', 'weeks', 'months', 'years'].include?(params[:time_unit]) ?  \
@@ -63,15 +62,17 @@ class NotificationsController < ApplicationController
       @notifications = @notifications.where('created_at >= ?', time.send(unit).ago)
     end
 
-    destroyed = @notifications.destroy_all
-
-    if destroyed.size.nonzero?
+    if @notifications.destroy_all.size.nonzero?
       flash[:success] = 'All notifications have been deleted.'
     else
       flash[:failed] = 'No notifications to delete.'
     end
 
     redirect_to action: :index
+  end
+
+  def set_user
+    @user = current_user
   end
 
 end
