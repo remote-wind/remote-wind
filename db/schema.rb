e1f3450e13304ea3b180fe21bf828b18038bc0e2
Max Calabrese <max.calabrese@ymail.com>
2014-03-13 11:40:57 +0100

    add confirmable attributes to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140226001904) do
+ActiveRecord::Schema.define(version: 20140313103821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -124,8 +124,13 @@ ActiveRecord::Schema.define(version: 20140226001904) do
     t.string   "image"
     t.string   "nickname"
     t.string   "slug"
+    t.string   "timezone"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["nickname"], name: "index_users_on_nickname", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
