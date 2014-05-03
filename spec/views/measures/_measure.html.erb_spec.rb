98cc0201c522124e62e928629be326582bdededa
Max Calabrese <max.calabrese@ymail.com>
2014-05-03 13:40:05 +0200

    more specs for measure partial

--- spec/views/measures/_measure.html.erb_spec.rb
+++ spec/views/measures/_measure.html.erb_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'measures/measure' do
 
   let(:station) { build_stubbed(:station) }
-  let(:measure) { build_stubbed(:measure) }
+  let(:measure) { build_stubbed(:measure, created_at: Time.new(2000) ) }
 
   subject(:row) do
     render partial: 'measures/measure',
@@ -12,7 +12,8 @@ describe 'measures/measure' do
   end
 
   it "has the correct data" do
-    expect(row).to have_selector '.measure'
+    expect(row).to have_selector '.created_at', text: "12/31 23:00"
+    expect(row).to have_selector '.speed', text: measure.speed
     expect(row).to have_selector '.speed', text: measure.speed
     expect(row).to have_selector '.direction', text: "E (90°)"
   end
