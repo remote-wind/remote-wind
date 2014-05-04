fdace58e2c8739f3ea9faa6c0a3170bf21a75364
Max Calabrese <max.calabrese@ymail.com>
2014-05-04 10:33:59 +0200

    use rspec generators

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,11 @@ module RemoteWind
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.generators do |g|
+      g.test_framework :rspec
+      g.integration_tool :rspec
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
 
   end
 end
