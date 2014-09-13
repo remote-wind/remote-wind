1e284f26c65446687d458093c2cf798e79f61e12
Max Calabrese <max.calabrese@ymail.com>
2014-09-13 15:51:10 +0200

    fix migration that fails due to precondition not being met

--- db/migrate/20140509165722_rename_station_down_to_offline.rb
+++ db/migrate/20140509165722_rename_station_down_to_offline.rb
@@ -1,5 +1,5 @@
 class RenameStationDownToOffline < ActiveRecord::Migration
   def change
-    rename_column :stations, :down, :offline
+    rename_column :stations, :down, :offline if Station.column_names.include?('down')
   end
 end
