2e8c6616f1942847f3b3cf1ad7a4d1f03bdcb3c3
Max Calabrese <max.calabrese@ymail.com>
2016-03-04 21:07:37 +0100

    fix spec for stations/find

--- spec/requests/stations_spec.rb
+++ spec/requests/stations_spec.rb
@@ -7,11 +7,10 @@ RSpec.describe "Stations", type: :request do
 
   describe "GET /stations/find/:hw_id" do
     it "finds a station by hardware id" do
-      get '/stations/find/' + station.hw_id
+      get '/stations/find/' + station.hw_id, format: :json
       expect(response).to have_http_status(200)
       expect(json).to eq({
-        "id" => station.id,
-        "hw_id" => station.hw_id
+        "id" => station.id
       })
     end
   end
